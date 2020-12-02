0888240d066adade2bec4f1568b0115563cd5a24
andrewp-as-is <russianidiot.github@gmail.com>
2020-12-02 16:19:44 +0300

    2020:12:02 16:19

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='django-manage',
+    version='2020.12.2',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages(),
     scripts=['scripts/manage.py']
